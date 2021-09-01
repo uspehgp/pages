f656fa40c1c1808069bb61fd0a59caa96781bfbe
uspehgp <uspehgp@gmail.com>
2021-09-01 21:51:06 +0300

    create paginator with logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -24,5 +24,21 @@ export class AppComponent {
       title: 'Stay shady',
       url: 'https://images.unsplash.com/photo-1521170813716-0b3f42fcfb65?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80'
     },
+    {
+      title: 'warm sunrise on beach',
+      url: 'https://images.unsplash.com/photo-1414609245224-afa02bfb3fda?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8Mnx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60'
+    },
+    {
+      title: 'San Lorenzo sunset reflected in the sea',
+      url: 'https://images.unsplash.com/photo-1473116763249-2faaef81ccda?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=773&q=80'
+    },
+    {
+      title: 'Editorial',
+      url: 'https://images.unsplash.com/photo-1520454974749-611b7248ffdb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=334&q=80'
+    },
+    {
+      title: 'Stay shady',
+      url: 'https://images.unsplash.com/photo-1521170813716-0b3f42fcfb65?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80'
+    },
   ]
 }
